1adc3a696b1a45403e967c72b3b9dd2cd7b00ed6
Shubham Aswal <aswals8991@gmail.com>
2021-06-02 20:51:11 +0530

    2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,38 @@
 import React, { useState } from 'react'
 
+
+const Filter = ({ filterText, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with a: <input value={filterText} onChange={handleFilterChange} placeholder='filter...' />
+    </div>
+  )
+}
+
+const PersonForm = ({ addPerson, newName, handleNameChange, handleNumberChange, newNumber }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} placeholder='a new name...' />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} placeholder='a new number...' />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Numbers = ({ personsToShow }) => {
+  return (
+    <div>
+      {personsToShow.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -7,6 +40,8 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
@@ -40,28 +75,17 @@ const App = () => {
     setPersonsToShow(personsToShowObject)
   }
 
-  const generateContacts = personsToShow.map(person => <p key={person.id}>{person.name} {person.number}</p>)
-
   return (
     <div>
-      <h1>Phonebook</h1>
-      <div>
-        filter shown with a: <input value={filterText} onChange={handleFilterChange} placeholder='filter...' />
-      </div>
-      <h2>add a person</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} placeholder='a new name...' />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} placeholder='a new number...' />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <h2>Phonebook</h2>
+      <Filter filterText={filterText} handleFilterChange={handleFilterChange} />
+      <h2>Add a new </h2>
+      <PersonForm addPerson={addPerson} newName={newName}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange} newNumber={newNumber}
+      />
       <h2>Numbers</h2>
-      {generateContacts}
+      <Numbers personsToShow={personsToShow} />
     </div>
   )
 }
