adc59cfe39c1da4e87083c6874d3c7b6297a33c4
Shubham Aswal <aswals8991@gmail.com>
2021-06-03 17:10:25 +0530

    3.10

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -76,7 +76,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`)
 })
